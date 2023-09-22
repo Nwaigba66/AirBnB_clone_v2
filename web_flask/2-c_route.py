9ecb703863620c45e5bd9dcea0457a6832574b8b
Thomas Collins <collinskthomas@gmail.com>
2023-09-22 10:34:54 +0000

    Add route c/<text> to flask app

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module starts a simple flask app
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def get_hello_hbnb():
+    """ Gets the message 'Hello HBNB!' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def get_hbnb():
+    """ Gets the message 'HBNB' """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def get_c_with_text(text):
+    """ Gets C <text>, replacing underscores with spaces """
+    text = text.replace('_', ' ')
+    return 'C ' + text
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
