db6cfe83be2f82486420f1c765070ad64a074993
Thomas Collins <collinskthomas@gmail.com>
2023-09-23 16:58:11 +0000

    Implement flask app to dynamically load state, city, amenities and places into webpage

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
+from models.user import User
 
 app = Flask(__name__)
 
@@ -36,15 +38,35 @@ def get_sorted_amenities():
     return sorted_amenities
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
+def get_sorted_places():
+    """
+    Returns sorted list of places
+    """
+    # Grab all places from storage
+    places = storage.all(Place).values()
+    # Sort all the places
+    sorted_places = sorted(places, key=lambda place: place.name)
+
+    # create the fullname of each place owner from their id
+    all_users = storage.all(User).values()
+    for place in sorted_places:
+        for user in all_users:
+            if user.id == place.user_id:
+                full_name = user.first_name + " " + user.last_name
+                place.owner = full_name
+    return sorted_places
+
+
+@app.route("/hbnb", strict_slashes=False)
 def get_filters():
     """
     Gets an HTML page which filters states & amenities
     """
     # Pass the sorted states and amenities to jinja template
-    return render_template('10-hbnb_filters.html',
+    return render_template('100-hbnb.html',
                            states=get_sorted_states(),
-                           amenities=get_sorted_amenities())
+                           amenities=get_sorted_amenities(),
+                           places=get_sorted_places())
 
 
 @app.teardown_appcontext
